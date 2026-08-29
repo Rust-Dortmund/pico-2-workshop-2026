1c4a24245d71a1af7da69764323367ee36728664
Domenic Quirl <DomenicQuirl@pm.me>
2026-08-29 12:24:19 +0200

    docs(ble): remove driver parameter comment indicator

--- 04c-wifi-ble/src/main.rs
+++ 04c-wifi-ble/src/main.rs
@@ -162,7 +162,6 @@ async fn main(spawner: Spawner) {
     info!("Initializing CYW43");
     let state = mk_static!(cyw43::State, cyw43::State::new());
     let cyw43_power = Output::new(PIN_23, Level::Low);
-    // NEW               vvvvvvvvvvvvvvvv
     // Gain access to the bluetooth driver by switching to `new_with_bluetooth`,
     // which requires passing the bluetooth firmware in addition to WiFi.
     let (network_device, bluetooth_driver, mut control, runner) =
